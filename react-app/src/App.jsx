ffb852342ee18eebf36ab4372055b7eca2d85aa8
Maks Haiduchok <145449886+JuMp1X@users.noreply.github.com>
2024-02-06 15:23:33 +0200

    create-post-form

---
+++ react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/header";
+import Infobar from "./components/infobar";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Infobar />
+    </>
+  );
+}
